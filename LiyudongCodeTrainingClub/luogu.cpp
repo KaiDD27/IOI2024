7b4a540ff7f75794fb60965448b9ab6666fa8107
GeekU <dinghaokai@gmail.com>
2024-01-25 22:35:41 +0800

    hash

--- LiyudongCodeTrainingClub/luogu.cpp
+++ LiyudongCodeTrainingClub/luogu.cpp
@@ -1,50 +1,66 @@
-#include <algorithm>
-#include <fstream>
+#include <cmath>
 #include <iostream>
-#include <stack>
+#include <queue>
 #include <vector>
+
 using namespace std;
 
-int M, N;
-int main() {
-  long long ans = 0;
-  stack<int> st;
-  cin >> N >> M;
-  vector<vector<char>> matrix(N + 2, vector<char>(M + 2));
-  vector<int> h(M + 2), r(M + 2), l(M + 2);
-  for (int n = 1; n <= N; n++)
-    for (int m = 1; m <= M; m++) {
-      cin >> matrix[n][m];
-    }
+class Solution {
+public:
+  vector<int> maxSlidingWindow(vector<int> &nums, int k, bool ascend) {
+    vector<int> ans;
+    while (!dq.empty())
+      dq.pop_back();
 
-  for (int n = 1; n <= N; n++) {
-    for (int m = 1; m <= M; m++) {
-      h[m] = matrix[n][m] == '*' ? 0 : h[m] + 1;
-    }
-    h[0] = 0;
-    h[M + 1] = 0;
-    for (int m = 1; m <= M + 1; m++) {
-      while (!st.empty() && h[m] < h[st.top()]) {
-        r[st.top()] = m;
-        st.pop();
+    for (int i = 0; i < nums.size(); i++) {
+      if (!dq.empty() && dq.front() <= i - k)
+        dq.pop_front();
+
+      if (ascend) {
+        while (!dq.empty() && nums[i] <= nums[dq.back()])
+          dq.pop_back();
+      } else {
+        while (!dq.empty() && nums[i] >= nums[dq.back()])
+          dq.pop_back();
       }
-      st.push(m);
+      dq.push_back(i);
+      if (i >= k - 1)
+        ans.push_back(nums[dq.front()]);
     }
-    while (!st.empty())
-      st.pop();
-    for (int m = M; m >= 0; m--) {
-      while (!st.empty() && h[m] <= h[st.top()]) {
-        l[st.top()] = m;
-        st.pop();
-      }
-      st.push(m);
-    }
-    for (int m = 1; m <= M; m++) {
-      ans += (m - l[m]) * (r[m] - m) * h[m];
-    }
-    //    cout << ans << endl;
+    return ans;
   }
 
+private:
+  deque<int> dq;
+};
+
+int A, B, N;
+int main() {
+  cin >> A >> B >> N;
+  vector<vector<int>> nums(A, vector<int>(B)),
+      maxNums(A, vector<int>(B - N + 1)), minNums(A, vector<int>(B - N + 1));
+  for (int i = 0; i < A; i++) {
+    for (int j = 0; j < B; j++) {
+      cin >> nums[i][j];
+    }
+  }
+  int ans = 1e9 + 1;
+  Solution solotion;
+  for (int i = 0; i < A; i++) {
+    minNums[i] = solotion.maxSlidingWindow(nums[i], N, true);
+    maxNums[i] = solotion.maxSlidingWindow(nums[i], N, false);
+  }
+  for (int i = 0; i < A - N + 1; i++) {
+    for (int j = 0; j < B - N + 1; j++) {
+      int maxVal = 0;
+      int minVal = 1e9;
+      for (int c = i; c < i + N; c++) {
+        maxVal = max(maxVal, maxNums[c][j]);
+        minVal = min(minVal, minNums[c][j]);
+      }
+      ans = min(ans, maxVal - minVal);
+    }
+  }
   cout << ans << endl;
   return 0;
 }
