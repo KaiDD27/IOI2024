751c32230aee953d8fbfe8a6509cefb994e8d1ac
GeekU <dinghaokai@gmail.com>
2024-01-31 09:49:12 +0800

    new book

---
+++ AlgorithmCompetition/luogu.cpp
@@ -0,0 +1,28 @@
+#include <cstddef>
+#include <iostream>
+using namespace std;
+
+struct Node {
+  int data;
+  Node *next;
+};
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  Node *head, *prev, *node, *now;
+
+  head = new Node;
+  head->data = 1;
+  head->next = nullptr;
+  now = head;
+
+  for (int i = 2; i <= n; i++) {
+    node = new Node;
+    node->data = i;
+    node->next = nullptr;
+    now->next = node;
+    now = node;
+  }
+  now->next = head;
+}
