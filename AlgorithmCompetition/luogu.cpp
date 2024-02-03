299afe8c3bfe5c47ca54578240cdfffc1fc271ff
GeekU <dinghaokai@gmail.com>
2024-02-03 22:02:48 +0800

    clangd

--- AlgorithmCompetition/luogu.cpp
+++ AlgorithmCompetition/luogu.cpp
@@ -1,22 +1,43 @@
 #include <iostream>
 #include <queue>
-#include <vector>
 using namespace std;
 const int INFN = 100005;
 const int INFM = 305;
-int groupId[INFN] = {-1};
-vector<queue<int>> twoDQueue;
-
+int groupId[INFN];
+int output[INFN];
+queue<int> queueArray[INFM];
+queue<queue<int> *> twoDQueue;
 int main() {
   int n, m, T;
   cin >> n >> m;
   for (int i = 0; i < n; i++) {
     cin >> groupId[i];
   }
+  cin >> T;
+  int cnt = 0;
 
   for (int i = 1; i <= T; i++) {
     string strOp;
     int factorId;
-    cin >> strOp >> factorId;
+    cin >> strOp;
+    if (strOp == "push") {
+      cin >> factorId;
+      if (queueArray[groupId[factorId]].empty())
+        twoDQueue.push(&queueArray[groupId[factorId]]);
+      queueArray[groupId[factorId]].push(factorId);
+    }
+    if (strOp == "pop") {
+
+      output[cnt] = twoDQueue.front()->front();
+      cnt++;
+      twoDQueue.front()->pop();
+      if (twoDQueue.front()->empty())
+        twoDQueue.pop();
+    }
   }
+
+  for (int i = 0; i < cnt; i++) {
+    cout << output[i] << endl;
+  }
+  return 0;
 }
