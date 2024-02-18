2a12312f66f3fc066880e19fb9a8b368acb8a4f2
GeekU <dinghaokai@gmail.com>
2024-02-18 08:56:26 +0800

    yestday

--- cses.cpp
+++ cses.cpp
@@ -1,22 +1,27 @@
+#include <cmath>
 #include <iostream>
-using namespace std;
+#include <vector>
 
+using namespace std;
+void generateGrayCode(vector<string> &grayCode, int n) {
+  if (n == 1) {
+    grayCode.push_back("0");
+    grayCode.push_back("1");
+    return;
+  }
+  generateGrayCode(grayCode, n - 1);
+  int halfCnt = (int)pow(2, n - 1);
+  for (int i = halfCnt - 1; i >= 0; i--) {
+    grayCode.push_back("1" + grayCode[i]);
+    grayCode[i] = "0" + grayCode[i];
+  }
+}
 int main() {
   int n;
   cin >> n;
-
-  if (n <= 3 && n != 1) {
-    cout << "NO SOLUTION";
-    return 0;
-  }
-
-  for (int i = 2; i <= n; i += 2) {
-    cout << i << " ";
-  }
-
-  for (int i = 1; i <= n; i += 2) {
-    cout << i << " ";
-  }
-
+  vector<string> grayCode;
+  generateGrayCode(grayCode, n);
+  for (int i = 0; i < (int)pow(2, n); i++)
+    cout << grayCode[i] << endl;
   return 0;
 }
