e5c09fa8c5e6435cdd0616755a0f7357ad35730f
GeekU <dinghaokai@gmail.com>
2024-02-19 08:15:05 +0800

    cses code

--- cses.cpp
+++ cses.cpp
@@ -1,16 +1,33 @@
+#include <algorithm>
 #include <iostream>
 #include <set>
 #include <string>
 
 using namespace std;
-int numsP[25];
-void group1() {}
+long long numsP[25];
+long long sum = 0;
+long long ans = 1000000000007;
+long long sumOfGroup1 = 0;
+int n;
+void calc(int j) {
+  if (sumOfGroup1 >= sum - sumOfGroup1) {
+    ans = min(ans, abs(2 * sumOfGroup1 - sum));
+    return;
+  }
+  for (int i = 1; i <= n; i++) {
+    sumOfGroup1 += numsP[i];
+    calc(i + 1);
+    sumOfGroup1 -= numsP[i];
+  }
+}
+
 int main() {
-  int n;
   cin >> n;
   for (int i = 1; i <= n; i++) {
     cin >> numsP[i];
+    sum += numsP[i];
   }
-
+  calc(0);
+  cout << ans << endl;
   return 0;
 }
