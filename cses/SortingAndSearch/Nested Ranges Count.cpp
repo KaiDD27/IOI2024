aba0d74abe8f863eeded514ca6e7daee3f97ff56
GeekU <dinghaokai@gmail.com>
2024-02-29 20:28:57 +0800

    BIT

---
+++ cses/SortingAndSearch/Nested Ranges Count.cpp
@@ -0,0 +1,88 @@
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <map>
+#include <set>
+using namespace std;
+using ll = long long;
+#define endl '\n'
+const int LEFT = 0;
+const int RIGHT = 1;
+const int INDEX = 2;
+vector<array<int, 3>> leftAsc;
+vector<int> bitTree;
+map<int, int> mpRightValToIndex;
+
+void updateBIT(int i, int valChange) {
+  while (i < bitTree.size()) {
+    bitTree[i] += valChange;
+    i += i & (-i);
+  }
+}
+
+int queryBIT(int i) {
+  int sum = 0;
+  while (i > 0) {
+    sum += bitTree[i];
+    i -= i & (-i);
+  }
+  return sum;
+}
+
+int main() {
+  ios::sync_with_stdio(false); // Fast I/O
+  cin.tie(nullptr); // Not safe to use cin/cout & scanf/printf together
+  int n;
+  cin >> n;
+  leftAsc.resize(n);
+  vector<int> isContained(n, 0), containsOther(n, 0);
+  set<int> rightSet;
+  for (int i = 0; i < n; i++) {
+    int l, r;
+    cin >> l >> r;
+    leftAsc[i][LEFT] = l;
+    leftAsc[i][RIGHT] = r;
+    leftAsc[i][INDEX] = i;
+    rightSet.insert(leftAsc[i][RIGHT]); // 获得 RIGHT 值形成的BIT的数组的个数
+  }
+  bitTree.resize(rightSet.size() + 1); // 所以从 1 开始所以，size 加 1
+  // 构造一个 Map 方便从 Right 的值查询对应到 BIT 数组的位置，注意 BIT 的索引从
+  // 1 开始
+  {
+    int iTree = 1;
+    for (auto val : rightSet) {
+      mpRightValToIndex[val] = iTree;
+      iTree++;
+    }
+  }
+  // 按照 Left 升序排序，Left相等，则Right 降序排序
+  sort(leftAsc.begin(), leftAsc.begin() + n,
+       [&](array<int, 3> a, array<int, 3> b) {
+         return a[LEFT] == b[LEFT] ? a[RIGHT] > b[RIGHT] : a[LEFT] < b[LEFT];
+       });
+  // 降序遍历 Left（Left 相等，则是升序遍历
+  // Right），这样可以确保找到所有的包含关系
+  for (int i = leftAsc.size() - 1; i >= 0; i--) {
+    int iTree = mpRightValToIndex[leftAsc[i][RIGHT]];
+    containsOther[leftAsc[i][INDEX]] = queryBIT(iTree);
+    updateBIT(iTree, 1);
+  }
+
+  // 升序遍历 Left（Left 相等，则是降序遍历
+  // Right），这样可以确保找到所有的被包含关系
+  fill(bitTree.begin(), bitTree.end(), 0);
+  for (int i = 0; i < leftAsc.size(); i++) {
+    int iTree = mpRightValToIndex[leftAsc[i][RIGHT]];
+    isContained[leftAsc[i][INDEX]] =
+        queryBIT(bitTree.size() - 1) - queryBIT(iTree - 1);
+    updateBIT(iTree, 1);
+  }
+
+  for (auto ans : containsOther)
+    cout << ans << " ";
+  cout << endl;
+  for (auto ans : isContained)
+    cout << ans << " ";
+
+  return 0;
+}
--- test/test.cpp
+++ test/test.cpp
@@ -1,16 +1,34 @@
-#include <algorithm>
+#include <functional>
 #include <iostream>
+#include <vector>
+
 using namespace std;
-int main() {
-  cout << "introduction" << endl;
-  while (true) {
-    string strTmp;
 
-    cin >> strTmp;
-    ///////////////////////
+function<double(double, double)> add = [](double x, double y) { return x + y; };
+function<double(double, double)> subtract = [](double x, double y) {
+  return x - y;
+};
+function<double(double, double)> multiply = [](double x, double y) {
+  return x * y;
+};
+function<double(double, double)> divide = [](double x, double y) {
+  return x / y;
+};
 
-    cout << rand() % 4 << " " << rand() % 4 << endl;
-    cout << strTmp + "!!!!" << endl;
+double calculate(vector<function<double(double, double)>> operations,
+                 vector<double> numbers) {
+  double result = numbers[0];
+  for (size_t i = 0; i < operations.size(); ++i) {
+    result = operations[i](result, numbers[i + 1]);
   }
+  return result;
+}
+
+int main() {
+  vector<function<double(double, double)>> operations = {add, subtract,
+                                                         multiply};
+  vector<double> numbers = {1.0, 2.0, 3.0, 4.0};
+  double result = calculate(operations, numbers);
+  cout << "Result: " << result << endl;
   return 0;
 }
