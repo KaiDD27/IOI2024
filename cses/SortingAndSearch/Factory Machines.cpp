e90027778466acbc5e43ea179bb688d06c307a4e
GeekU <dinghaokai@gmail.com>
2024-03-24 18:51:20 +0800

    binary search

--- cses/SortingAndSearch/Factory Machines.cpp
+++ cses/SortingAndSearch/Factory Machines.cpp
@@ -26,13 +26,22 @@ int main() {
   }
   // 只有一台机器生产t 个产品所需的时间应该是上限
   ll l = 1, r = t * k[0];
+  // 其实就是一个lowerBound，大于等于t的第一个取值
   while (l < r) {
-    ll mid = (l + r) >> 1; // 向下取整
+    ll mid = (l + r) >> 1; // 向下取整，才能保证不在r=mid死循环
     if (calcProductCount(mid) < t)
       l = mid + 1; // 因middle是向下取整，所以l加1，反之就是r要减1
-    else
+    else // r不能等于mid-1，因为这样可能会错过唯一相等的值
       r = mid;
   }
+  /*
+  while (l <= r) {
+    ll mid = (l + r) >> 1;
+    if (calcProductCount(mid) < t)
+      l = mid + 1;
+    else
+      r = mid - 1;
+  }*/
   cout << l << endl;
   return 0;
 }
