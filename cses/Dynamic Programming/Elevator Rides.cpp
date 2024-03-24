23b706309fc802713304097c853dd9a4ccd4301b
GeekU <dinghaokai@gmail.com>
2024-03-24 10:22:51 +0800

    elevatorrides

--- cses/Dynamic Programming/Elevator Rides.cpp
+++ cses/Dynamic Programming/Elevator Rides.cpp
@@ -13,10 +13,10 @@ int main() {
   cin.tie(nullptr); // Not safe to use cin/cout & scanf/printf together
   int n, x;
   cin >> n >> x;
-  w.resize(n + 1);
-  for (int i = 1; i <= n; i++)
+  w.resize(n);
+  for (int i = 0; i < n; i++)
     cin >> w[i];
-  dp.resize(1 << n, {n, x}); // 最大值是n+1个电梯，且装满了x
+  dp.resize(1 << n, {n, x}); // 最大值是n个电梯，且装满了x
   dp[0] = {1, 0};
   for (int iMask = 1; iMask < dp.size(); iMask++) {
     // 用最笨的方法遍历所有的先后次序，则一定可以找到最优解，最优解就是在一个组合里面最少的电梯趟数且最后一个电梯重量最小
@@ -24,11 +24,11 @@ int main() {
     // 在某一个iMask组合下，最后任何一个组合里面的人都可能是最后一个上电梯的人，所有都要考虑，取最小值即可
     // 巧妙的利用了二进制递推时，大的数总是任何位上面的 1 变成 0
     // 都可以在前面找到，这样确保了转移总是从未知向已知转移
-    for (int i = 1; i <= n; i++) {
-      if (iMask & 1 << (i - 1)) // 说明第 i 个人在这个组合里面
+    for (int i = 0; i < n; i++) {
+      if (iMask & 1 << i) // 说明第 i 个人在这个组合里面
       {
-        auto prev = dp[iMask & ~(1 << (i - 1))]; // 去掉第 i 个人的组合
-        if (prev.second + w[i] > x) {            // 装不下则新开趟电梯
+        auto prev = dp[iMask ^ (1 << i)]; // 去掉第 i 个人的组合
+        if (prev.second + w[i] > x) {     // 装不下则新开趟电梯
           dp[iMask] = min(dp[iMask], {prev.first + 1, w[i]});
         } else {
           dp[iMask] = min(dp[iMask], {prev.first, prev.second + w[i]});
--- cses/Dynamic Programming/test.cpp
+++ cses/Dynamic Programming/test.cpp
@@ -1,12 +1,46 @@
+#include <algorithm>
+#include <cstring>
 #include <iostream>
-
+#include <vector>
 using namespace std;
-using ll = long long;
-#define endl "\n"
+#define int long long
 
-int main() {
-  ios::sync_with_stdio(false); // Fast I/O
-  cin.tie(nullptr); // Not safe to use cin/cout & scanf/printf together
-  
+int dp[20][10][2][2]; // DP array with dimensions for position, previous digit,
+                      // leading zeroes, and tight
+
+// Function to compute the number of valid numbers up to a given digit
+int solve(const string &num, int pos, int prev, bool leading_zero, bool tight) {
+  if (pos == num.size())
+    return 1; // Base case: if at the end, count as 1 valid number
+  if (dp[pos][prev][leading_zero][tight] != -1)
+    return dp[pos][prev][leading_zero][tight]; // Memoization check
+
+  int limit =
+      tight ? (num[pos] - '0') : 9; // Determine the limit for the current digit
+  int ways = 0;
+
+  for (int dig = 0; dig <= limit; ++dig) {
+    if (dig == prev && !leading_zero)
+      continue; // Skip if the digit is the same as the previous non-zero digit
+    ways += solve(num, pos + 1, dig, leading_zero && dig == 0,
+                  tight && dig == limit);
+  }
+
+  return dp[pos][prev][leading_zero][tight] =
+             ways; // Store and return the computed value
+}
+
+int countValidNumbers(int n) {
+  string num = to_string(n);
+  memset(dp, -1, sizeof(dp));          // Initialize DP array with -1
+  return solve(num, 0, 0, true, true); // Start the DP with the first digit
+}
+
+int32_t main() {
+  int a, b;
+  cin >> a >> b;
+  int res = countValidNumbers(b) -
+            (a ? countValidNumbers(a - 1) : 0); // Calculate result for [a, b]
+  cout << res << endl;
   return 0;
 }
