37dbd7e9817cca9b862655e6ba243f485547e51e
GeekU <dinghaokai@gmail.com>
2024-03-25 23:01:35 +0800

    two set 2

--- cses/Dynamic Programming/Two Sets II.cpp
+++ cses/Dynamic Programming/Two Sets II.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <utility>
 #include <vector>
@@ -16,14 +15,20 @@ int main() {
   cin.tie(nullptr); // Not safe to use cin/cout & scanf/printf together
   int n;
   cin >> n;
+  // 如果是是奇数则无法平分，则无解
+  int sumN = (1 + n) * n / 2;
+  if (sumN % 2 != 0) {
+    cout << "0" << endl;
+    return 0;
+  }
   // 将问题转化从i 个数的选出任意个数，让他们的和为某一个值，有多少种可能性，当
   // i 等于n，且和为 n 个数之和的一半的时候，这样的可能性除以 2 就是这题的答案
-  dp.resize(n + 1, vector<pair<int, int>>((n + 1) * n / 2 + 1));
+  dp.resize(n + 1, vector<pair<int, int>>(sumN + 1));
   dp[0][0].first = 1;
   for (int i = 1; i <= n; i++) {
     int sumI = (1 + i) * i / 2;
     for (int j = 0; j <= sumI; j++) {
-      // 大于的都可以转化为选剩下的那部分
+      // 和大于sum/2的都可以转化为选剩下的那部分
       if (j > sumI / 2) {
         dp[i][j] = dp[i][sumI - j];
         continue;
@@ -32,8 +37,8 @@ int main() {
       if (i > j) {
         dp[i][j] = dp[j][j];
       } else if (i <= j) {
-        dp[i][j] = dp[i - 1][j - i];          // i 被选中
-        dp[i][j].first += dp[i - 1][j].first; // i 没有被选中
+        dp[i][j] = dp[i - 1][j - i];          // i被选中可能性
+        dp[i][j].first += dp[i - 1][j].first; // 累加i没有被选中可能性
 
         dp[i][j].second += (dp[i][j].first / MOD);
         dp[i][j].second += dp[i - 1][j].second;
@@ -43,10 +48,6 @@ int main() {
       }
     }
   }
-  int sumN = (1 + n) * n / 2;
-  if (sumN % 2 == 0) {
-    cout << (dp[n][sumN / 2].first + dp[n][sumN / 2].second * MOD) / 2 << endl;
-  } else // 无解
-    cout << "0" << endl;
+  cout << (dp[n][sumN / 2].first + dp[n][sumN / 2].second * MOD) / 2 << endl;
   return 0;
 }
