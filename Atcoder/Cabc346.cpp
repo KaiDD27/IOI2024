67866275f45b15cab1e588b41ed561ee2f61bd43
GeekU <dinghaokai@gmail.com>
2024-04-25 21:24:12 +0800

    346

--- Atcoder/Cabc346.cpp
+++ Atcoder/Cabc346.cpp
@@ -1,9 +1,12 @@
+#include <array>
 #include <iostream>
 #include <set>
+#include <vector>
+
 using namespace std;
 using ll = long long;
 #define endl "\n"
-set<ll> seta;
+set<ll> setN;
 int main() {
   ios::sync_with_stdio(false); // Fast I/O
   cin.tie(nullptr); // Not safe to use cin/cout & scanf/printf together
@@ -11,14 +14,14 @@ int main() {
   cin >> n >> k;
   ll sum = k * (k + 1) / 2;
   for (int i = 0; i < n; i++) {
-    ll ai;
-    cin >> ai;
-    if (ai <= k) {
-      seta.insert(ai);
-    }
+    ll ni = 0;
+    cin >> ni;
+    if (ni <= k)
+      setN.insert(ni);
   }
-  for (auto ai : seta) {
-    sum -= ai;
+
+  for (auto si : setN) {
+    sum -= si;
   }
   cout << sum << endl;
   return 0;
