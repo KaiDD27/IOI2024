f37b42c217e6fd0a07f2a9a1a2587203ab58d1df
GeekU <dinghaokai@gmail.com>
2024-05-22 13:19:03 +1200

    2019-3-5

--- NZOI/2019-3-5.cpp
+++ NZOI/2019-3-5.cpp
@@ -1,20 +1,93 @@
+#include <algorithm>
 #include <array>
+#include <climits>
 #include <iostream>
+#include <queue>
 #include <vector>
-
 using namespace std;
-using ll = long long;
-#define endl "\n"
-ll n, k;
-vector<ll> x;
-int main() {
-  ios::sync_with_stdio(false); // Fast I/O
-  cin.tie(nullptr); // Not safe to use cin/cout & scanf/printf together
-  // n<=65536,k<=16
-  cin >> n >> k;
-  x.resize(n + 1);
-  for (int i = 1; i <= n; i++)
-    cin >> x[i];
 
-  return 0;
+int n, k;
+vector<int> skills;
+vector<vector<int>> strongerCnt;
+int simulate(int p, int i, int j) {
+  int wins = 0;
+  for (int r = 1; r <= k; r++) {
+    // 属于同一个区的比Toddy 强的选手数量
+    int currStrongerCnt = strongerCnt[r][p >> r];
+    // 交换最多只能减少一个强手，所以如果大于 1，则一定没法赢了
+    if (currStrongerCnt > 1)
+      break;
+    // Adjust to account for swap of players i and j
+    if (p >> r == i >> r) {
+      // Player i within sub-bracket
+      currStrongerCnt += strongerCnt[0][j] - strongerCnt[0][i];
+    }
+    if (p >> r == j >> r) {
+      // Player j within sub-bracket
+      currStrongerCnt += strongerCnt[0][i] - strongerCnt[0][j];
+    }
+    if (currStrongerCnt == 0) {
+      wins++;
+    } else {
+      break;
+    }
+  }
+  return wins;
+}
+
+int main() {
+  cin >> n >> k;
+  skills.resize(n);
+  strongerCnt.resize(k + 1, vector<int>(n));
+  for (int i = 0; i < n; i++)
+    cin >> skills[i];
+
+  int firstLoss = 0;
+  int maxWins = 0;
+  int ways = 0;
+  // 遍历，第一个比
+  // Toddy强的，不一定是会遇上Toddy，如果遇不上，那么将和别人交换就无益处，一定有人在这个区赢
+  // Toddy，对结果没影响，所以不需要找出第一个真正赢了 Toddy 的人。
+  for (int i = 1; i < n; i++) {
+    skills[i] = skills[i] > skills[0];
+    if (skills[i] && !firstLoss)
+      firstLoss = i;
+    strongerCnt[0][i] = skills[i];
+  }
+  skills[0] = 0;
+  strongerCnt[0][0] = 0;
+
+  for (int r = 1; r <= k; r++) {
+    for (int m = 0; m < (n >> r); m++) {
+      strongerCnt[r][m] =
+          strongerCnt[r - 1][m * 2] + strongerCnt[r - 1][m * 2 + 1];
+    }
+  }
+
+  int initialWins = simulate(0, 0, 0);
+  for (int i = 0; i < n; i++) {
+    // Try swapping Toddy with player i
+    int wins = simulate(i, 0, i);
+    if (wins > maxWins) {
+      maxWins = wins;
+      ways = 1;
+    } else if (wins != 0 && wins == maxWins) {
+      ways++;
+    }
+
+    // Try swapping the first player Toddy loses to with player i
+    if (firstLoss) {
+      wins = simulate(0, firstLoss, i);
+      if (wins > maxWins) {
+        maxWins = wins;
+        ways = 1;
+      } else if (wins == maxWins) {
+        ways++;
+      }
+    }
+  }
+
+  if (initialWins == maxWins)
+    ways = -1;
+  cout << maxWins << endl << ways << endl;
 }
--- test.cpp
+++ test.cpp
@@ -1,28 +1,83 @@
 #include <algorithm>
+#include <array>
+#include <climits>
 #include <iostream>
+#include <queue>
 #include <vector>
 using namespace std;
 
+int n, k;
+vector<int> skills;
+vector<vector<int>> stronger;
+int simulate(int p, int i, int j) {
+  int wins = 0;
+  for (int r = 1; r <= k; r++) {
+    int stronger_players = stronger[r][p >> r];
+    // Adjust to account for swap of players i and j
+    if (p >> r == i >> r) {
+      // Player i within sub-bracket
+      stronger_players += stronger[0][j] - stronger[0][i];
+    }
+    if (p >> r == j >> r) {
+      // Player j within sub-bracket
+      stronger_players += stronger[0][i] - stronger[0][j];
+    }
+    if (stronger_players == 0)
+      wins++;
+  }
+  return wins;
+}
+
 int main() {
-  int N;
-  long long c;
-  cin >> N >> c;
-  vector<long long> hs(N + 1); // + 1 so we don't overrun k + 1
-  for (int j = 0; j < N; j++)
-    cin >> hs[j];
+  cin >> n >> k;
+  skills.resize(n);
+  stronger.resize(k + 1, vector<int>(n));
+  for (int i = 0; i < n; i++)
+    cin >> skills[i];
 
-  sort(hs.begin(), hs.end(), std::greater<long long>());
+  int firstLoss = 0;
+  int maxWins = 0;
+  int ways = 0;
 
-  long long n_deployed = 0;
-  for (int k = 0; k < N; k++) {
-    int i = k + 1; // calculate first non-profitable height or the next
-                   // tree height if all heights in between are profitable
-    long long h = std::max(c / (i * 5), hs[k + 1]);
+  for (int i = 1; i < n; i++) {
+    skills[i] = skills[i] > skills[0];
+    if (skills[i] && !firstLoss)
+      firstLoss = i;
+    stronger[0][i] = skills[i];
+  }
+  skills[0] = 0;
+  stronger[0][0] = 0;
 
-    // add profitable heights between current height and h
-    if (h < hs[k])
-      n_deployed += hs[k] - h;
+  for (int r = 1; r <= k; r++) {
+    for (int m = 0; m < (n >> r); m++) {
+      stronger[r][m] = stronger[r - 1][m * 2] + stronger[r - 1][m * 2 + 1];
+    }
   }
 
-  cout << n_deployed << endl;
+  int initialWins = simulate(0, 0, 0);
+  for (int i = 1; i < n; i++) {
+    // Try swapping Toddy with player i
+    int wins = simulate(i, 0, i);
+    if (wins > maxWins) {
+      maxWins = wins;
+      ways = 1;
+    } else if (wins != 0 && wins == maxWins) {
+      ways++;
+    }
+
+    // Try swapping the first player Toddy loses to with player i
+    if (firstLoss) {
+      wins = simulate(0, firstLoss, i);
+      if (wins > maxWins) {
+        maxWins = wins;
+        ways = 1;
+      } else if (wins == maxWins) {
+        ways++;
+      }
+    }
+  }
+
+  if (initialWins == maxWins)
+    ways = -1;
+  cout << maxWins << endl << ways << endl;
 }
