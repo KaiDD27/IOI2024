607b79216e7b30d0c0cb71f06b0934df7a456f62
GeekU <dinghaokai@gmail.com>
2024-06-01 22:45:24 +0800

    2021-3-4

--- NZOI/2021-3-4.cpp
+++ NZOI/2021-3-4.cpp
@@ -1,53 +1,91 @@
+#include <algorithm>
 #include <array>
 #include <iostream>
-#include <map>
-#include <tuple>
+#include <iterator>
+#include <set>
 #include <vector>
 
 using namespace std;
 using ll = long long;
 #define endl "\n"
-vector<bool> cOfficial;
 vector<tuple<char, ll>> queryM;
 vector<ll> cRank;
-map<ll, ll> mpOffical;
+set<ll> stNoOffical;
+set<ll> stNoOffical5000050050;
 int main() {
   ios::sync_with_stdio(false); // Fast I/O
   cin.tie(nullptr); // Not safe to use cin/cout & scanf/printf together
   ll n, m;
   cin >> n >> m;
-  cOfficial.resize(n + 1, true);
-  cRank.resize(n + 1, 0);
   queryM.resize(m);
+  bool subtask5 = false;
   for (auto &[chM, idx] : queryM) {
     cin >> chM >> idx;
-    if (chM == 'o')
-      mpOffical[idx] = idx;
+    if (chM == 'o' && idx > 50050 & m > 500)
+      subtask5 = true;
   }
+  if (subtask5 == true) {
+    return 0;
+  }
+  if (m <= 500) {
+    for (auto [chM, idx] : queryM) {
+      if (chM == 't') {
+        if (stNoOffical.count(idx))
+          stNoOffical.erase(idx);
+        else
+          stNoOffical.insert(idx);
+      }
+      if (chM == 'o') {
+        if (stNoOffical.count(idx))
+          cout << "UNOFFICIAL" << endl;
+        else {
+          if (stNoOffical.empty())
+            cout << idx << endl;
+          else {
+            auto it = stNoOffical.upper_bound(idx);
+            cout << idx - distance(stNoOffical.begin(), it) << endl;
+          }
+        }
+      }
+    }
+    return 0;
+  }
+
+  ll below50000Cnt = 0;
   for (auto [chM, idx] : queryM) {
     if (chM == 't') {
-      cOfficial[idx] = !cOfficial[idx];
-      if (cOfficial[idx] == false) {
-        mpOffical.erase(idx);
-        auto it = mpOffical.upper_bound(idx);
-        while (it != mpOffical.end()) {
-          it->second--;
+      if (idx < 50000) {
+        if (stNoOffical.count(idx)) {
+          stNoOffical.erase(idx);
+          below50000Cnt--;
+        } else {
+          stNoOffical.insert(idx);
+          below50000Cnt++;
+        }
+      } else if (idx <= 50050) {
+        if (stNoOffical5000050050.count(idx)) {
+          stNoOffical5000050050.erase(idx);
+        } else {
+          stNoOffical5000050050.insert(idx);
         }
       } else {
+        break;
       }
     }
     if (chM == 'o') {
-      if (cOfficial[idx] == false)
+      if (stNoOffical.count(idx) || stNoOffical5000050050.count(idx))
         cout << "UNOFFICIAL" << endl;
       else {
-        ll rank = 0;
-        for (int i = 1; i <= idx; i++) {
-          rank += cOfficial[i];
+        if (stNoOffical5000050050.empty())
+          cout << idx - below50000Cnt << endl;
+        else {
+          auto it = stNoOffical5000050050.upper_bound(idx);
+          cout << idx - below50000Cnt -
+                      distance(stNoOffical5000050050.begin(), it)
+               << endl;
         }
-        cout << rank << endl;
       }
     }
   }
-
   return 0;
 }
