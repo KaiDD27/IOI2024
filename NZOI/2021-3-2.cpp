37a348be342d0a2be1778cb12655c8620a34adf7
GeekU <dinghaokai@gmail.com>
2024-06-01 09:05:43 +0800

    2021-3-2

--- NZOI/2021-3-2.cpp
+++ NZOI/2021-3-2.cpp
@@ -11,17 +11,16 @@ int main() {
   cin.tie(nullptr); // Not safe to use cin/cout & scanf/printf together
   ll n, a, b;
   cin >> n >> a >> b;
-  if (a * 3 <= b * 2)
+  if (a * 3 <= b * 2) {
     if (n % 2 == 0)
       cout << n / 2 * a << endl;
     else
       cout << (n - 3) / 2 * a + b << endl;
-  else {
+  } else {
     if (n % 2 == 0)
       cout << n / 6 * b * 2 + n % 6 / 2 * a << endl;
     else
       cout << b + (n - 3) / 6 * b * 2 + (n - 3) % 6 / 2 * a << endl;
   }
-
   return 0;
 }
