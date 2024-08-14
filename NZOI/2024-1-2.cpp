d38e7bd9fb7761d8c296e7833ba21787a104b3c4
GeekU <dinghaokai@gmail.com>
2024-08-14 12:22:12 +0800

    2024-1-2

--- NZOI/2024-1-2.cpp
+++ NZOI/2024-1-2.cpp
@@ -7,29 +7,38 @@
 using namespace std;
 using ll = long long;
 #define endl "\n"
-// 求最大公约数,辗转相除法
+
+// 计算最大公约数（GCD）
 int gcd(int a, int b) { return b == 0 ? a : gcd(b, a % b); }
-// 求最小公倍数
+
+// 计算最小公倍数（LCM）
 int lcm(int a, int b) { return a * b / gcd(a, b); }
+
 int main() {
-  ios::sync_with_stdio(false); // Fast I/O
-  cin.tie(nullptr); // Not safe to use cin/cout & scanf/printf together
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
   ll a, b, k;
   cin >> a >> b >> k;
+
+  // 确保 a 始终大于或等于 b
   if (a < b)
     swap(a, b);
-  ll miniDist = k, miniCnt = 0;
-  ll miniMultiple = lcm(a, b);
-  ll MultiDividA = miniMultiple / a;
-  ll MultiDividB = miniMultiple / b;
 
+  ll miniDist = k, miniCnt = 0;
+  ll miniMultiple = lcm(a, b);       // 计算 a 和 b 的最小公倍数
+  ll MultiDividA = miniMultiple / a; // a 在最小公倍数中的倍数
+  ll MultiDividB = miniMultiple / b; // b 在最小公倍数中的倍数
+
+  // 遍历所有可能的 a 和 b 的组合
   for (ll i = 0; i <= MultiDividA; i++) {
     for (ll j = 0; j <= MultiDividB; j++) {
       ll sum = i * a + j * b;
       if (sum == 0)
         continue;
-      ll result;
-      ll dist;
+
+      ll result, dist;
+      // 计算距离 k 的最小距离和所需的倍数
       if ((k % sum) <= (sum - k % sum)) {
         result = k / sum;
         dist = (k % sum);
@@ -37,8 +46,11 @@ int main() {
         result = k / sum + 1;
         dist = (sum - k % sum);
       }
-      ll aCnt = i * result;
-      ll bCnt = j * result;
+
+      ll aCnt = i * result; // a 的总数
+      ll bCnt = j * result; // b 的总数
+
+      // 更新最小距离和最小数量
       if (miniDist > dist) {
         miniDist = dist;
         miniCnt =
