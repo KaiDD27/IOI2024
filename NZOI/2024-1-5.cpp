199bb7d30ea111cfef74fb2d2795161d376f5879
GeekU <dinghaokai@gmail.com>
2024-08-16 09:29:50 +0800

    2024-1-5

--- NZOI/2024-1-5.cpp
+++ NZOI/2024-1-5.cpp
@@ -12,7 +12,8 @@ vector<vector<ll>> adj, dp;
 // dfsNextButNotSubTree是dfs
 // 先序遍历里面排在当前节点之后,且不是当前节点的子节点的第一个节点，其实就是先序遍历排在当前节点后面的第一个兄弟节点
 void dfs(int a, int dfsNextButNotSubTree, int father) {
-  // curr_Next是先序遍历当前节点的下一个节点，当这个节点不是子节点的时候，等于dfsNextButNotSubTree
+  // curr_Next可以作为先序遍历当前节点的下一个节点，且curr_Next节点的 dp
+  // 都已经算好了，可以确保状态转移成功。当这个节点不是子节点的时候，等于dfsNextButNotSubTree
   int curr_Next = dfsNextButNotSubTree;
   for (auto b : adj[a]) {
     if (b == father)
@@ -33,6 +34,12 @@ int main() {
   t.resize(n);
   adj.resize(n);
   dp.resize(n + 1, vector<ll>(m + 1, 5000));
+  // 这行代码初始化了dp数组
+  // dp是一个二维vector，大小为(n+1) x (m+1)
+  // n+1是因为我们需要包含从0到n的所有节点
+  // m+1是因为我们需要考虑从0到m的所有可能的时间
+  // 所有元素初始化为5000，这是一个足够大的值，用作初始的最大值
+  // 在后续的动态规划过程中，我们会更新这些值为实际的最小甜度
   for (auto &ti : t)
     cin >> ti;
 
