aa411c85efdd12f7ea80f9eddcc5f2fa4f37e3ce
GeekU <dinghaokai@gmail.com>
2024-08-18 16:35:10 +0800

    apikey

---
+++ apikey.js
@@ -0,0 +1,2 @@
+export const NOTION_API_SECRET = 'secret_6x7Ye8Jj5STJHiWCruElRuAsThLK3ZaEeJBsEyVEO8m';
+//export const NOTION_API_SECRET = 'secret_H3lojnu0r2IBH6g9hylonaGmlbN7owxFpptlE4qa0Mn';//test
