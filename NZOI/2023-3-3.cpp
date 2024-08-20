bb44410e154758ad353d06e3b39b5289f58aedc2
GeekU <dinghaokai@gmail.com>
2024-08-20 13:18:49 +0800

    2023-3-3

--- NZOI/2023-3-3.cpp
+++ NZOI/2023-3-3.cpp
@@ -1,62 +1,74 @@
-#include <array>
 #include <cmath>
 #include <iomanip>
 #include <iostream>
 #include <queue>
-#include <utility>
 #include <vector>
-
 using namespace std;
 using ll = long long;
-#define endl "\n"
-vector<array<double, 3>> tree;
-vector<vector<pair<double, ll>>> adj;
-double sX, sY, tX, tY;
+
+// Constants
 const int X = 0, Y = 1, R = 2;
-ll n;
-double distanceOfTowP(const array<double, 3> &a, const array<double, 3> &b) {
-  double ret =
-      sqrt((a[X] - b[X]) * (a[X] - b[X]) + (a[Y] - b[Y]) * (a[Y] - b[Y])) -
-      a[R] - b[R];
-  if (ret > 0)
-    return ret;
-  else
-    return 0;
+
+// Global variables
+vector<array<double, 3>> trees;
+vector<vector<pair<double, ll>>> adj;
+double startX, startY, endX, endY;
+ll numTrees;
+
+// Function implementations
+double distanceBetweenPoints(const array<double, 3> &a,
+                             const array<double, 3> &b) {
+  double dx = a[X] - b[X];
+  double dy = a[Y] - b[Y];
+  double distance = sqrt(dx * dx + dy * dy) - a[R] - b[R];
+  return max(0.0, distance);
 }
-// BFS求解最短路径
+
 double shortestPath() {
-  vector<double> dist(n + 2, 1e8);
+  vector<double> dist(numTrees + 2, 1e8);
   dist[0] = 0;
   queue<ll> q;
   q.push(0);
+
   while (!q.empty()) {
-    ll idA = q.front();
+    ll current = q.front();
     q.pop();
-    for (auto [distAB, idB] : adj[idA]) {
-      if (dist[idA] + distAB < dist[idB]) {
-        dist[idB] = dist[idA] + distAB;
-        q.push(idB);
+
+    for (const auto &[distance, neighbor] : adj[current]) {
+      if (dist[current] + distance < dist[neighbor]) {
+        dist[neighbor] = dist[current] + distance;
+        q.push(neighbor);
       }
     }
   }
-  return dist[n + 1];
+
+  return dist[numTrees + 1];
 }
 int main() {
-  ios::sync_with_stdio(false); // Fast I/O
-  cin.tie(nullptr); // Not safe to use cin/cout & scanf/printf together
-  cin >> sX >> sY >> tX >> tY;
-  cin >> n;
-  tree.resize(n + 2);
-  adj.resize(n + 2);
-  tree[0][X] = sX, tree[0][Y] = sY, tree[n + 1][X] = tX, tree[n + 1][Y] = tY;
-  for (int i = 1; i <= n; i++)
-    cin >> tree[i][X] >> tree[i][Y] >> tree[i][R];
-  for (int i = 0; i <= n + 1; i++) {
-    for (int j = 0; j <= n + 1; j++) {
-      adj[i].push_back({distanceOfTowP(tree[i], tree[j]), j});
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  // Input
+  cin >> startX >> startY >> endX >> endY >> numTrees;
+  trees.resize(numTrees + 2);
+  adj.resize(numTrees + 2);
+
+  trees[0] = {startX, startY, 0};
+  trees[numTrees + 1] = {endX, endY, 0};
+
+  for (int i = 1; i <= numTrees; i++) {
+    cin >> trees[i][X] >> trees[i][Y] >> trees[i][R];
+  }
+
+  // Build adjacency list
+  for (int i = 0; i <= numTrees + 1; i++) {
+    for (int j = 0; j <= numTrees + 1; j++) {
+      adj[i].push_back({distanceBetweenPoints(trees[i], trees[j]), j});
     }
   }
-  cout << fixed << setprecision(6) << shortestPath() << endl;
+
+  // Output result
+  cout << fixed << setprecision(6) << shortestPath() << "\n";
 
   return 0;
 }
