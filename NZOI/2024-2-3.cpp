3059bd6df2e62d2f1e0f9347c73541ab9140c9b0
GeekU <dinghaokai@gmail.com>
2024-08-21 13:17:10 +0800

    2024-2-3 map<ll,map<ll,ll>>

--- NZOI/2024-2-3.cpp
+++ NZOI/2024-2-3.cpp
@@ -9,23 +9,22 @@ using ll = long long;
 #define endl "\n"
 int n;
 ll a;
-map<ll, vector<pair<ll, int>>> rows;
+map<ll, map<ll, ll>> rows;
 int main() {
   cin >> n >> a;
   for (int i = 0; i < n; i++) {
     ll r, c;
     cin >> r >> c;
     for (ll ri = max(0LL, r - a); ri < r + a + 1; ri++) {
-      rows[ri].push_back({max(0LL, c - a), 1});
-      rows[ri].push_back({c + a + 1, -1});
+      rows[ri][max(0LL, c - a)] += 1;
+      rows[ri][c + a + 1] -= 1;
     }
   }
   ll res = 0;
-  for (auto row : rows) {
-    sort(row.second.begin(), row.second.end());
-    int cnt = 0;
+  for (auto &row : rows) {
+    ll cnt = 0;
     ll prev = 0;
-    for (auto cur : row.second) {
+    for (auto &cur : row.second) {
       if (cnt > 0) {
         res += cur.first - prev;
       }
